f656829054187997b75e6158bf73b918aaa532f3
Eunmi Lee <eunoia0235@gmail.com>
2025-04-18 07:10:29 +0900

    feat: provider 삭제

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { state, ThemeContext } from './context/ThemeContext';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ThemeContext.Provider value={state}>
-      <App />
-    </ThemeContext.Provider>
+    <App />
   </React.StrictMode>
 );
