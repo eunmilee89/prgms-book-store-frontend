41f914ca74af4ed0996b040c1bef98c94f367699
Eunmi Lee <eunoia0235@gmail.com>
2025-04-19 04:24:31 +0900

    test: InputText 렌더링, forwardRef 테스트 코드 추가

---
+++ src/components/common/InputText.spec.tsx
@@ -0,0 +1,30 @@
+import { render, screen } from '@testing-library/react';
+import InputText from './InputText';
+import { BookStoreThemeProvider } from '../../context/ThemeContext';
+import React from 'react';
+
+describe('InputText 컴포넌트 테스트', () => {
+  it('렌더링 확인', () => {
+    render(
+      <BookStoreThemeProvider>
+        <InputText placeholder='여기에 입력하세요' />
+      </BookStoreThemeProvider>
+    );
+
+    expect(
+      screen.getByPlaceholderText('여기에 입력하세요')
+    ).toBeInTheDocument();
+  });
+
+  it('forwardRef 테스트', () => {
+    const ref = React.createRef<HTMLInputElement>();
+
+    render(
+      <BookStoreThemeProvider>
+        <InputText placeholder='여기에 입력하세요' ref={ref} />
+      </BookStoreThemeProvider>
+    );
+
+    expect(ref.current).toBeInstanceOf(HTMLInputElement);
+  });
+});
