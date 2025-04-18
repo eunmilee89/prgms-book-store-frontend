ab76b2bcd728084c2a4e172364919654968bdca8
Eunmi Lee <eunoia0235@gmail.com>
2025-04-19 04:23:15 +0900

    feat: heading, button, buttonScheme, borderRadius 스타일 추가

--- src/style/theme.ts
+++ src/style/theme.ts
@@ -1,9 +1,38 @@
 export type ThemeName = 'light' | 'dark';
-type ColorKey = 'primary' | 'background' | 'secondary' | 'third';
+export type ColorKey =
+  | 'primary'
+  | 'background'
+  | 'secondary'
+  | 'third'
+  | 'border'
+  | 'text';
+export type HeadingSize = 'large' | 'medium' | 'small';
+export type ButtonSize = 'large' | 'medium' | 'small';
+export type ButtonScheme = 'primary' | 'normal';
 
 export interface Theme {
   name: ThemeName;
   color: Record<ColorKey, string>;
+  heading: {
+    [key in HeadingSize]: {
+      fontSize: string;
+    };
+  };
+  button: {
+    [key in ButtonSize]: {
+      fontSize: string;
+      padding: string;
+    };
+  };
+  buttonScheme: {
+    [key in ButtonScheme]: {
+      color: string;
+      backgroundColor: string;
+    };
+  };
+  borderRadius: {
+    default: string;
+  };
 }
 
 export const light: Theme = {
@@ -13,16 +42,59 @@ export const light: Theme = {
     background: 'lightgrey',
     secondary: 'blue',
     third: 'green',
+    border: 'grey',
+    text: 'black',
+  },
+  heading: {
+    large: {
+      fontSize: '2rem',
+    },
+    medium: {
+      fontSize: '1.5rem',
+    },
+    small: {
+      fontSize: '1rem',
+    },
+  },
+  button: {
+    large: {
+      fontSize: '1.5rem',
+      padding: '1rem 2rem',
+    },
+    medium: {
+      fontSize: '1rem',
+      padding: '0.5rem 1rem',
+    },
+    small: {
+      fontSize: '0.75rem',
+      padding: '0.25em 0.5rem',
+    },
+  },
+  buttonScheme: {
+    primary: {
+      color: 'white',
+      backgroundColor: 'midnightblue',
+    },
+    normal: {
+      color: 'black',
+      backgroundColor: 'lightgrey',
+    },
+  },
+  borderRadius: {
+    default: '4px',
   },
 };
 
 export const dark: Theme = {
+  ...light,
   name: 'dark',
   color: {
     primary: 'coral',
     background: 'midnightblue',
     secondary: 'darkblue',
     third: 'darkgreen',
+    border: 'grey',
+    text: 'white',
   },
 };
 
