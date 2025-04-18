f1c6a38c2024a4e325d0fa798d09e6093b934a57
Eunmi Lee <eunoia0235@gmail.com>
2025-04-19 04:25:53 +0900

    feat: Title 컴포넌트 추가

---
+++ src/components/common/Title.tsx
@@ -0,0 +1,18 @@
+import { styled } from 'styled-components';
+import { ColorKey, HeadingSize, Theme } from '../../style/theme';
+
+interface Props {
+  children: React.ReactNode;
+  size: HeadingSize;
+  color?: ColorKey;
+}
+
+export default function Title({ children, size }: Props) {
+  return <TitleStyle size={size}>{children}</TitleStyle>;
+}
+
+const TitleStyle = styled.h1<Omit<Props, 'children'>>`
+  font-size: ${({ theme, size }) => (theme as Theme).heading[size].fontSize};
+  color: ${({ theme, color }) =>
+    color ? (theme as Theme).color[color] : (theme as Theme).color.primary};
+`;
