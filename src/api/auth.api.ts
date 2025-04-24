a48ba40f2b127b7b1bb0a1dc055fef859f844e0e
Eunmi Lee <eunoia0235@gmail.com>
2025-04-25 03:17:02 +0900

    feat: /users 데이터 불러오는 비동기함수 추가

--- src/api/auth.api.ts
+++ src/api/auth.api.ts
@@ -5,3 +5,22 @@ export const signup = async (userData: SignUpProps) => {
   const response = await httpClient.post('/users/join', userData);
   return response.data;
 };
+
+export const resetRequest = async (data: SignUpProps) => {
+  const response = await httpClient.post('/users/reset', data);
+  return response.data;
+};
+
+export const resetPassword = async (data: SignUpProps) => {
+  const response = await httpClient.put('/users/reset', data);
+  return response.data;
+};
+
+interface LoginResponse {
+  token: string;
+}
+
+export const login = async (data: SignUpProps) => {
+  const response = await httpClient.post<LoginResponse>('/users/login', data);
+  return response.data;
+};
