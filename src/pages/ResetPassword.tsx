161ea24a4b219b15f7392d7b77b71e296e60235f
Eunmi Lee <eunoia0235@gmail.com>
2025-04-25 03:24:04 +0900

    feat: 비밀번호 초기화 컴포넌트

---
+++ src/pages/ResetPassword.tsx
@@ -0,0 +1,81 @@
+import Title from '../components/common/Title';
+import InputText from '../components/common/InputText';
+import Button from '../components/common/Button';
+import { Link, useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { resetPassword, resetRequest } from '../api/auth.api';
+import { useAlert } from '../hooks/useAlert';
+import { StyledSignup } from './Signup';
+import { useState } from 'react';
+
+export interface SignUpProps {
+  email: string;
+  password: string;
+}
+
+export default function ResetPassword() {
+  const navigate = useNavigate();
+  const showAlert = useAlert();
+  const [resetRequested, setResetRequested] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignUpProps>();
+
+  const onSubmit = (data: SignUpProps) => {
+    if (resetRequested) {
+      // 초기화
+      resetPassword(data).then(() => {
+        showAlert('비밀번호가 초기화되었습니다.');
+        navigate('/login');
+      });
+    } else {
+      // 요청
+      resetRequest(data).then(() => {
+        setResetRequested(true);
+      });
+    }
+  };
+
+  return (
+    <>
+      <Title size='large'>비밀번호 초기화</Title>
+      <StyledSignup>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <fieldset>
+            <InputText
+              placeholder='이메일'
+              inputType='email'
+              {...register('email', { required: true })}
+            />
+            {errors.email && (
+              <p className='error-text'>이메일을 입력해주세요.</p>
+            )}
+          </fieldset>
+          {resetRequested && (
+            <fieldset>
+              <InputText
+                placeholder='비밀번호'
+                inputType='password'
+                {...register('password', { required: true })}
+              />
+              {errors.password && (
+                <p className='error-text'>비밀번호를 입력해주세요.</p>
+              )}
+            </fieldset>
+          )}
+          <fieldset>
+            <Button type='submit' size='medium' scheme='primary'>
+              {resetRequested ? '비밀번호 초기화' : '초기화 요청'}
+            </Button>
+          </fieldset>
+          <div className='info'>
+            <Link to='/reset'>비밀번호 초기화</Link>
+          </div>
+        </form>
+      </StyledSignup>
+    </>
+  );
+}
