56da2f80a504817eefe566c8405f6f89ab1c566f
Eunmi Lee <eunoia0235@gmail.com>
2025-04-29 01:57:49 +0900

    feat: 주문 api 올리기, 가져오기, 상세 주문 가져오기 추가

---
+++ src/api/order.api.ts
@@ -0,0 +1,19 @@
+import { Order, orderDetailItem, OrderSheet } from '../models/order.model';
+import { httpClient } from './http';
+
+export const order = async (orderData: OrderSheet) => {
+  const response = await httpClient.post('/orders', orderData);
+  return response.data;
+};
+
+export const fetchOrders = async () => {
+  const response = await httpClient.get<Order[]>('/orders');
+  return response.data;
+};
+
+export const fetchOrder = async (orderId: number) => {
+  const response = await httpClient.get<orderDetailItem[]>(
+    `/orders/${orderId}`
+  );
+  return response.data;
+};
