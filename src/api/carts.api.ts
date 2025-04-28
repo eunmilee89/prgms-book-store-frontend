153ed8b8747342fc564b71c522ac2968c1e20d64
Eunmi Lee <eunoia0235@gmail.com>
2025-04-29 01:56:57 +0900

    feat: 장바구니 api 가져오기, 삭제하기 추가

--- src/api/carts.api.ts
+++ src/api/carts.api.ts
@@ -1,3 +1,4 @@
+import { Cart } from '../models/cart.model';
 import { httpClient } from './http';
 
 interface AddCartParams {
@@ -7,5 +8,15 @@ interface AddCartParams {
 
 export const addCart = async (params: AddCartParams) => {
   const response = await httpClient.post(`/carts`, params);
+  return response.data;
+};
+
+export const fetchCart = async () => {
+  const response = await httpClient.get<Cart[]>('/carts');
+  return response.data;
+};
+
+export const deleteCart = async (cardId: number) => {
+  const response = await httpClient.delete(`/carts/${cardId}`);
   return response.data;
 };
