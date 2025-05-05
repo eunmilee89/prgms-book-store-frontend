0ba1dfe31a5b1dcbd007e053a9819465c5db00f5
Eunmi Lee <eunoia0235@gmail.com>
2025-05-05 23:11:34 +0900

    style: 스타일 추가

--- src/components/header/ThemeSwitcher.tsx
+++ src/components/header/ThemeSwitcher.tsx
@@ -1,8 +1,26 @@
 import { useContext } from 'react';
 import { ThemeContext } from '../../context/ThemeContext';
+import styled from 'styled-components';
+import { Theme } from '@/style/theme';
 
 export default function ThemeSwitcher() {
   const { themeName, toggleTheme } = useContext(ThemeContext);
 
-  return <button onClick={toggleTheme}>{themeName}</button>;
+  return (
+    <StyledThemeSwitcher onClick={toggleTheme}>{themeName}</StyledThemeSwitcher>
+  );
 }
+
+const StyledThemeSwitcher = styled.button`
+  font-size: 1rem;
+  font-weight: 600;
+  text-decoration: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  line-height: 1;
+  background: none;
+  border: 0;
+  cursor: pointer;
+`;
